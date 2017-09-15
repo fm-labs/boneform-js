6de08b6115ff960b375830172b0e53cc5d23e6a1
fm-labs <flowmotionlabs@gmail.com>
2017-09-15 23:57:31 +0200

    updated submit btn

--- src/submit.js
+++ src/submit.js
@@ -18,46 +18,29 @@
             options = _.extend({}, options);
 
             this.form = options.form || this.form || undefined;
-            this.key = options.key || this.key || 'submit';
 
             this.options = _.extend({
                 // field
-                idPrefix: '',
                 label: '',
-                help: '',
 
                 control: {},
                 controlAttrs: {}
-                //fieldAttrs: {},
-            }, _.omit(options, ['key', 'form']));
-
-            this.errors = [];
-
-            // generate control id
-            this.controlId = this.options.idPrefix + this.key;
+            }, _.omit(options, ['form']));
 
             // create control
             var ctrlOpts = _.extend({
                 form: this.form,
-                data: null,
                 field: this,
-                id: this.controlId,
-                name: this.key,
                 attrs: this.options.controlAttrs,
 
-                label: this.label,
+                label: this.options.label
             }, this.options.control);
             this.control = new Backbone.Form.controls['Button'](ctrlOpts);
         },
 
         render: function() {
 
-            // render field and attach control listeners
-            var $field = this.template({
-                controlId: this.controlId,
-                help: this.options.help || this.options.label || this.key + ' : ' + this.options.type,
-                label: this.options.label || this.key
-            });
+            var $field = this.template({});
             this.setElement($field);
 
             this.$el.find('[data-control]').replaceWith(this.control.render().el);
